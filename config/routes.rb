fc66b022746a1bcbf9958d792417154e00696c1c
tsuyoshi_anazawa <anatsuyo0221@gmail.com>
2019-04-11 15:36:54 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+	resources :blogs
+
+   'blogs/show'
+   'blogs/index'
+   'blogs/new'
+   'blogs/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
